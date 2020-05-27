c53693e22655fca365c791b639364cd4dff76d4b
Alyssa Nycum <alyssa.nycum@gmail.com>
2020-05-27 10:43:43 -0500

    working on payment type list in settings

--- src/components/customers/Settings.js
+++ src/components/customers/Settings.js
@@ -1,8 +1,42 @@
 //affordances to add and delete payment type
 
-import React from 'react';
+import React, { useEffect } from 'react';
 import PaymentTypeCard from '../payments/PaymentTypeCard';
-import PaymentCardManager from '../../modules/PaymentTypeManager';
+import PaymentTypeManager from '../../modules/PaymentTypeManager';
+import useSimpleAuth from '../auth/useSimpleAuth';
 
+const PaymentTypeList = (props) => {
+    const [paymentTypes, setPaymentTypes] = useState([]);
+    const { isAuthenticated } = useSimpleAuth();
 
+    const getPaymentTypes = () => {
+        if (isAuthenticated()) {
+            PaymentTypeManager.getPaymentTypesByUser()
+                .then(paymentTypes => {
+                    setPaymentTypes(paymentTypes)
+                })
+        }
+    };
+
+    useEffect(() => {
+        getPaymentTypes();
+    }, []);
+
+    return (
+        <div>
+            <h1>Saved Payment Types:</h1>
+            <div>
+                {paymentTypes.map(paymentType =>
+                    <PaymentTypeCard
+                        key={paymentType.id}
+                        paymentType={paymentType}
+                        { ...props }
+                    />
+                )}
+            </div>
+        </div>
+    )
+};
+
+export default PaymentTypeList;
 
