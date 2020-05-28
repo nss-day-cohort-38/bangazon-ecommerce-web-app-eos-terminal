275f35ad121dd947ae3b8ff52b4eb131c42d7e98
CoopNich <CoopNich92@gmail.com>
2020-05-28 15:21:09 -0500

    search results can now be linked to details. minor styling on product types

--- src/components/products/ProductTypeCard.js
+++ src/components/products/ProductTypeCard.js
@@ -19,15 +19,14 @@ const ProductTypeCard = props => {
             <div>
                 <h3>{props.productType.name} ({finalProductAmount})</h3>
             
-            <p>
+        
                 <ul>
                     {props.productType.products.map(product =>
-                        <a className="detail-link"onClick={() => props.history.push(`/products/${parseInt(product.url.split('/')[4])}`)}><li>{product.title} ({product.quantity})</li></a>
+                        <a className="detail-link"onClick={() => props.history.push(`/products/${parseInt(product.url.split('/')[4])}`)}><strong><li>{product.title} ({product.quantity})</li></strong></a>
                         
                         )
                     }
                 </ul>
-            </p>
             </div>
         </>
     )
