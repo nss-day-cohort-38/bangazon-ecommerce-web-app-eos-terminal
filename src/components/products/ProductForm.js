63b10782d3c8c9ba6e4254f62ac1243860211d85
kwohl <ktwohl@gmail.com>
2020-05-28 14:37:16 -0500

    added local delivery toggle display

--- src/components/products/ProductForm.js
+++ src/components/products/ProductForm.js
@@ -1,11 +1,12 @@
 //form to add/sell a product
 import React, { useEffect, useState } from "react"
 import ProductManager from "../../modules/ProductManager"
-
+import './ProductForm.css'
 
 const ProductForm = props => {
   const [newProduct, setNewProduct] = useState({ title: "", price: 0.01, description: "", quantity: 1, location: "", image: "", productTypeId: 0 });
   const [productTypes, setProductTypes] = useState([]);
+  const [toDisplay, setToDisplay] = useState(true);
 
   const handleFieldChange = (evt) => {
     const stateToChange = { ...newProduct };
@@ -100,9 +101,9 @@ const ProductForm = props => {
       </fieldset>
       <fieldset>
         <label> Local Delivery Available? </label>
-        <label><input type="radio" name="deliveryOption" value="no" defaultChecked /> No </label>
-        <label><input type="radio" name="deliveryOption" value="yes" /> Yes </label>
-        <div className="hidden">
+        <label><input onClick={() => setToDisplay(true)} type="radio" name="deliveryOption" value="no" defaultChecked /> No </label>
+        <label><input onClick={() => setToDisplay(false)} type="radio" name="deliveryOption" value="yes" /> Yes </label>
+        <div style={{display:  toDisplay ? 'none' : ''}} >
         <label htmlFor="location"> Location </label>
         <input onChange={handleFieldChange} type="text"
           id="location"
