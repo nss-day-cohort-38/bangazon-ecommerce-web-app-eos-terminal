465ec4ed394e33954150788c5fbb87f9e605ea19
CoopNich <CoopNich92@gmail.com>
2020-06-03 10:36:12 -0500

    lameeeee

--- src/modules/ProductManager.js
+++ src/modules/ProductManager.js
@@ -1,24 +1,23 @@
 const authApiUrl = "http://localhost:8000"
-
 export default {
-  addProduct(newProduct) {
-    return fetch(`${authApiUrl}/products`, {
-      "method": "POST",
-      "headers": {
-        "Content-Type": "application/json",
-        "Accept": "application/json",
-        "Authorization": `Token ${sessionStorage.getItem("bangazon-token")}`
-      },
-      "body": JSON.stringify(newProduct)
-    })
-      .then(response => response.json())
-  },
-  getProductTypes() {
-    return fetch(`${authApiUrl}/producttypes`)
-        .then(response => response.json())
-  },
-  getProductById(productId) {
-    return fetch(`${authApiUrl}/products/${productId}`)
-        .then(resp => resp.json())
-  }
+    addProduct(newProduct) {
+        return fetch(`${authApiUrl}/products`, {
+            "method": "POST",
+            "headers": {
+                "Content-Type": "application/json",
+                "Accept": "application/json",
+                "Authorization": `Token ${sessionStorage.getItem("bangazon-token")}`
+            },
+            "body": JSON.stringify(newProduct)
+        })
+          .then(response => response.json())
+    },
+    getProductTypes() {
+        return fetch(`${authApiUrl}/producttypes`)
+            .then(response => response.json())
+    },
+    getProductById(productId) {
+        return fetch(`${authApiUrl}/products/${productId}`)
+            .then(resp => resp.json())
+    }
 }
