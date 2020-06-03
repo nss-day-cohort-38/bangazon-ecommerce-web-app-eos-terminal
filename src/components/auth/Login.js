6a808eb13c78622005e9a2c3f7a4e8bf542f89ae
castle <castlesmadeofcode@gmail.com>
2020-06-03 10:55:51 -0500

    fixed issie with spacing for home page

--- src/components/auth/Login.js
+++ src/components/auth/Login.js
@@ -1,10 +1,12 @@
-  
-import React, { useState } from "react"
+import React, { useState } from "react";
 import useSimpleAuth from "./useSimpleAuth";
 
-const Login = props => {
-  const [credentials, setCredentials] = useState({ username: "", password: "" });
-  const { login } = useSimpleAuth()
+const Login = (props) => {
+  const [credentials, setCredentials] = useState({
+    username: "",
+    password: "",
+  });
+  const { login } = useSimpleAuth();
 
   const handleFieldChange = (evt) => {
     const stateToChange = { ...credentials };
@@ -12,44 +14,51 @@ const Login = props => {
     setCredentials(stateToChange);
   };
 
-  const handleLogin = e => {
+  const handleLogin = (e) => {
     e.preventDefault();
 
     const customerCreds = {
-      "username": credentials.username,
-      "password": credentials.password
-    }
+      username: credentials.username,
+      password: credentials.password,
+    };
 
-    login(customerCreds)
-      .then(() => props.history.push("/ "))
-  }
+    login(customerCreds).then(() => props.history.push("/"));
+  };
 
   return (
     <div className="content">
-    <form className="form--login" onSubmit={handleLogin}>
-      <h1 className="h3 mb-3 font-weight-normal">Login to Bangazon!</h1>
-      <fieldset>
-        <label htmlFor="username"> Username </label>
-        <input onChange={handleFieldChange} type="text"
-          id="username"
-          placeholder="Username"
-          required="" autoFocus="" value={credentials.username}/>
-      </fieldset>
-      <fieldset>
-        <label htmlFor="password"> Password </label>
-        <input onChange={handleFieldChange} type="password"
-          id="password"
-          placeholder="Password"
-          required="" autoFocus="" value={credentials.password}/>
-      </fieldset>
-      <fieldset>
-        <button type="submit">
-          Login
-                    </button>
-      </fieldset>
-    </form>
+      <form className="form--login" onSubmit={handleLogin}>
+        <h1 className="h3 mb-3 font-weight-normal">Login to Bangazon!</h1>
+        <fieldset>
+          <label htmlFor="username"> Username </label>
+          <input
+            onChange={handleFieldChange}
+            type="text"
+            id="username"
+            placeholder="Username"
+            required=""
+            autoFocus=""
+            value={credentials.username}
+          />
+        </fieldset>
+        <fieldset>
+          <label htmlFor="password"> Password </label>
+          <input
+            onChange={handleFieldChange}
+            type="password"
+            id="password"
+            placeholder="Password"
+            required=""
+            autoFocus=""
+            value={credentials.password}
+          />
+        </fieldset>
+        <fieldset>
+          <button type="submit">Login</button>
+        </fieldset>
+      </form>
     </div>
-  )
-}
+  );
+};
 
 export default Login;
