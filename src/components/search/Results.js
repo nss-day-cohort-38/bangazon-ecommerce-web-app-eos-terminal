3a20ec65fa9e2b5ec81dabb9f2afffc425d25770
Matthew Kroeger <mattk932009@gmail.com>
2020-06-05 10:23:34 -0500

    image shows in search 75x75

--- src/components/search/Results.js
+++ src/components/search/Results.js
@@ -37,13 +37,23 @@ const SearchResults = (props) => {
     <div className="content">
       <h3>{results.length} product(s) found matching your search</h3>
       {results.map(result => (
+        <>
+         <picture>
+         <img className="searchImage" src={result.image} alt=""/>
+        </picture>
         <strong><li key={result.id} onClick={() => props.history.push(`/products/${result.id}`)}>{result.title}</li></strong>
+        </>
       ))}
 
       <h3>{local.length} product(s) found with location matching your search</h3>
       
       {local.map(result => (
+        <>
+         <picture>
+         <img className="searchImage" src={result.image} alt=""/>
+        </picture>
         <strong><li key={result.id} onClick={() => props.history.push(`/products/${result.id}`)}>{result.title}</li></strong>
+        </>
       ))}
     </div>
     )
