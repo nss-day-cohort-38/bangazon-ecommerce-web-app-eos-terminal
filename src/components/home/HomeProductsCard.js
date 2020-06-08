efa53f90ef65120811346756977a3cf25847a248
CoopNich <CoopNich92@gmail.com>
2020-06-08 13:36:51 -0500

    styling for no image

--- src/components/home/HomeProductsCard.js
+++ src/components/home/HomeProductsCard.js
@@ -6,7 +6,11 @@ const HomeProductsCard = (props) => {
   return (
     <div className="product-card">
         <div className="product-image">
-          <img src={props.product.image}></img>
+          {
+            props.product.image
+          ?<img src={props.product.image}></img>
+          : null
+          }
         </div>
         <div className="product-info" key={props.product.id}>
           <a onClick={() => { props.history.push(`/products/${props.product.id}`); }}>{props.product.title}</a>
