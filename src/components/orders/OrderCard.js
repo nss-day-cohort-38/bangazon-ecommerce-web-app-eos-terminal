74e8411082dd7d7aa04d5e85a009b9dd72dd025a
kwohl <ktwohl@gmail.com>
2020-06-09 10:09:18 -0500

    removed orderId from order card in order history

--- src/components/orders/OrderCard.js
+++ src/components/orders/OrderCard.js
@@ -6,7 +6,6 @@ const OrderCard = (props) => {
     <div className="card">
       <div className="card-content">
         <h3>Order {props.orderCount}</h3>
-        <p>Order ID: {props.order.id}</p>
         <p>Created On: {props.order.created_at.split("T")[0]}</p>
         <p>Payment Type: {props.order.payment_type.merchant_name}</p>
         <button
