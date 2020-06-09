2cce8b496da673a14728eefce49eb2ebfe0cf60d
kwohl <ktwohl@gmail.com>
2020-06-09 10:00:16 -0500

    refactored cart and order details

--- src/components/orders/Cart.js
+++ src/components/orders/Cart.js
@@ -1,11 +1,14 @@
 // Cart (current open order)
 import React, { useState, useEffect } from "react";
 import OrderManager from "../../modules/OrderManager";
+import OrderProductManager from "../../modules/OrderProductManager";
+import ProductManager from "../../modules/ProductManager";
 
 const Cart = (props) => {
 
   const [order, setOrder] = useState({ url: "", created_at: "", payment_type: "" });
   const [isLoading, setIsLoading] = useState(false);
+  const [products, setProducts] = useState([])
 
   const getCurrentOrder = () => {
     return OrderManager.getAll()
@@ -17,18 +20,45 @@ const Cart = (props) => {
         });
     };
 
-    const handleDelete = () => {
-        setIsLoading(true);
-        OrderManager.delete(order.id).then(() =>
-            props.history.push("/cart"),
-            setIsLoading(false),
-            setOrder({ url: "", created_at: "", payment_type: "" })
-        );
-        };  
-
+  const getOrderProducts = () => {
+    const productArray = []
+    return OrderProductManager.getOrderProducts()
+      .then(orderProductsResp => {
+        orderProductsResp.map(product => {
+          if(parseInt(product.order.url.split('/')[4]) === order.id) {
+            productArray.push(product)
+          }
+        })
+        setProducts(productArray)
+      })
+  }
+  const handleDelete = () => {
+      setIsLoading(true);
+      OrderManager.delete(order.id).then(() =>
+          props.history.push("/cart"),
+          setIsLoading(false),
+          setOrder({ url: "", created_at: "", payment_type: "" })
+      );
+      };  
+  
+  const handleOPDelete = (product_id, orderProduct_id, quantity) => {
+    setIsLoading(true);
+    products.map(product => {
+      if(parseInt(product.product.url.split('/')[4]) === product_id) {
+        orderProduct_id = product.id
+        quantity = product.product.quantity
+      }
+    })
+    OrderProductManager.deleteOrderProduct(orderProduct_id)
+      .then(ProductManager.updateProductQuantity({quantity: quantity + 1, id: product_id}))
+        .then(() =>
+          props.history.push("/categories"))
+  };
+  
   useEffect(() => {
     getCurrentOrder()
-  }, []);
+      .then(getOrderProducts())
+  }, [order.id]);
 
   if (order.payment_type === null && order) {
     return (
@@ -36,7 +66,17 @@ const Cart = (props) => {
         <div className="content">
             Welcome to your cart.
             <p>Your cart was opened on {order.created_at.split('T')[0]}</p>
-        </div>
+        <div>{products.map(product => 
+          <li key={product.id}>
+            <span>Title: {product.product.title}</span>
+            <span> -- Price: ${product.product.price}</span>
+            <button 
+            type="button"
+            disabled={isLoading}
+            onClick={() => handleOPDelete(parseInt(product.product.url.split('/')[4]))}>Remove</button>
+           </li>
+        )}</div>
+        <br /><br />
         <div>
         <button
         type="button"
@@ -47,6 +87,7 @@ const Cart = (props) => {
         <button type="button" disabled={isLoading} onClick={handleDelete}>
           Cancel Order
         </button>
+        </div>
         </div>
         </>
       ) 
--- src/components/orders/OrderDetails.js
+++ src/components/orders/OrderDetails.js
@@ -1,40 +1,23 @@
 import React, { useState, useEffect } from "react";
 import OrderManager from "../../modules/OrderManager";
 import OrderProductManager from "../../modules/OrderProductManager";
-import ProductManager from "../../modules/ProductManager";
 
 const OrderDetail = props => {
   const [order, setOrder] = useState({ name: "" });
   const [products, setProducts] = useState([]);
-  const [isLoading, setIsLoading] = useState(true);
+  const [date, setDate] = useState("")
 
   const getUserProducts = () => {
+    const productArray = []
     OrderProductManager.getOrderProducts().then(orderProducts => {
-      setProducts(orderProducts)
+      orderProducts.map(orderProduct => {
+        if(parseInt(orderProduct.order.url.split('/')[4]) === props.orderId) {
+          productArray.push(orderProduct)
+        }
+      })
+      setProducts(productArray)
     })
   } 
-  
-  const handleDelete = () => {
-    //invoke the delete function in OrderManger and re-direct to the order list.
-    setIsLoading(true);
-    OrderManager.delete(props.orderId).then(() =>
-      props.history.push("/order")
-    );
-  };
-
-  const handleOPDelete = (product_id, orderProduct_id, quantity) => {
-    setIsLoading(true);
-    products.map(product => {
-      if(parseInt(product.product.url.split('/')[4]) === product_id) {
-        orderProduct_id = product.id
-        quantity = product.product.quantity
-      }
-    })
-    OrderProductManager.deleteOrderProduct(orderProduct_id)
-      .then(ProductManager.updateProductQuantity({quantity: quantity + 1, id: product_id}))
-        .then(() =>
-          props.history.push("/order"))
-  };
 
   useEffect(() => {
     getUserProducts()
@@ -43,67 +26,44 @@ const OrderDetail = props => {
       setOrder({
         created_at: order.created_at
       });
-      setIsLoading(false);
+      setDate(order.created_at.split('T')[0])
     });
+
   }, [props.orderId]);
 
-  if (order.payment_type !== null) {
-    return (
-      <div className="content">
-      <div className="card">
-        <div className="card-content">
-          <picture></picture>
-          <h3>
-            Order ID: <span style={{ color: "darkslategrey" }}>{props.orderId}</span>
-          </h3>
-          <h3>
-            Created At: <span style={{ color: "darkslategrey" }}>{order.created_at}</span>
-          </h3>
-        </div>
-      </div>
-      </div>
-    )
-  } else {
     return (
     <div className="content">
     <div className="card">
       <div className="card-content">
+      <div>    <button
+          type="button"
+          onClick={() => {
+            props.history.push("/orderhistory")
+          }}
+        >Back
+        </button> </div>
         <picture></picture>
         <h3>
           Order ID: <span style={{ color: "darkslategrey" }}>{props.orderId}</span>
         </h3>
         <h3>
-          Created At: <span style={{ color: "darkslategrey" }}>{order.created_at}</span>
+          Created On: <span style={{ color: "darkslategrey" }}>{date}</span>
         </h3>
         <ul>
-        <div>{products.map(product => 
-        <>
+        <div>
+        {products.map(product => 
           <li key={product.id}>
             <span>Title: {product.product.title}</span>
             <span> -- Price: ${product.product.price}</span>
-            <button 
-            type="button"
-            disabled={isLoading}
-            onClick={() => handleOPDelete(parseInt(product.product.url.split('/')[4]))}>Remove</button>
-           </li></>
-        )}</div>
+          </li>
+        )}
+        </div>
         </ul>
-
-        <button
-          type="button"
-          onClick={() => props.history.push(`/order/${props.orderId}/edit`)}
-        >
-          Complete Order
-        </button>
-        <button type="button" disabled={isLoading} onClick={handleDelete}>
-          Cancel Order
-        </button>
 
       </div>
     </div>
     </div>
   );
-  }
 };
 
 export default OrderDetail;
